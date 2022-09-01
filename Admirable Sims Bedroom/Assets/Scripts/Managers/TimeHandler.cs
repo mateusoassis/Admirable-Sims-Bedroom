157b628fe40426719b4e545d983f0b2bc0c79c25
DESKTOP-8PG3LDG\Mateus <mateusoassis@gmail.com>
2022-09-01 09:36:18 -0300

    Features: - Time handler implemented and fully functional

--- Admirable Sims Bedroom/Assets/Scripts/Managers/TimeHandler.cs
+++ Admirable Sims Bedroom/Assets/Scripts/Managers/TimeHandler.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TimeHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private int hours;
+    [SerializeField] private int minutes;
+    [SerializeField] private float timeMultiplier; // every X seconds, 1 minute passes
+    private float timer;
+    [SerializeField] private TextMeshProUGUI timeTextHolder;
+
     void Start()
     {
-        
+        timeTextHolder.text = hours.ToString("00") + ":" + minutes.ToString("00");
+        timer = timeMultiplier;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            UpdateTime();
+        }
+    }
+
+    private void UpdateTime()
+    {
+        minutes++;
+        if(minutes == 60)
+        {
+            minutes = 0;
+            hours++;
+            if(hours == 24)
+            {
+                hours = 0;
+            }
+        }
+        timeTextHolder.text = hours.ToString("00") + ":" + minutes.ToString("00");
+        timer = timeMultiplier;
     }
 }
