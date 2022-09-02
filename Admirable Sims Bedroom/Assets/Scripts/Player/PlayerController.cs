49c81ea599b682c13e821871383796dd14253d5e
DESKTOP-8PG3LDG\Mateus <mateusoassis@gmail.com>
2022-09-02 15:01:35 -0300

    Feature: - Player control logic switching between states is fine

--- Admirable Sims Bedroom/Assets/Scripts/Player/PlayerController.cs
+++ Admirable Sims Bedroom/Assets/Scripts/Player/PlayerController.cs
@@ -8,9 +8,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     private Vector2 movementVector;
 
+    public bool isFacingRight;
+
+    public GameObject[] directions; // 0 sides, 1 top, 2 bottom
+
     void Update()
     {
         StoredPlayerInput();
+        HandleAnimation();
     }
 
     void FixedUpdate()
@@ -24,12 +29,71 @@ public class PlayerController : MonoBehaviour
     private void StoredPlayerInput()
     {
         float x = Input.GetAxisRaw("Horizontal");
+        
+        
         float y = Input.GetAxisRaw("Vertical");
+
         movementVector = new Vector2(x, y).normalized;
+
+        if(x > 0.9)
+        {
+            isFacingRight = true;
+            EnableSides();
+        }
+        else if(x < -0.9)
+        {
+            isFacingRight = false;
+            EnableSides();
+        }
+        
+        if(x == 0)
+        {
+            if(y > 0.9)
+            {
+                EnableTop();
+            }
+            else if(y < -0.9)
+            {
+                EnableBottom();
+            }
+        }
     }
 
     private void MovePlayer()
     {
         rb.velocity = new Vector2(movementVector.x * p_moveSpeed, movementVector.y * p_moveSpeed);
+    }
+
+    public void HandleAnimation()
+    {
+        // which side is player looking
+        if(isFacingRight)
+        {
+            this.transform.localScale = new Vector3(0.8f, 0.8f, 0.8f);
+        }
+        else
+        {
+            
+            this.transform.localScale = new Vector3(-0.8f, 0.8f, 0.8f);
+        }   
+    }
+
+    public void EnableSides()
+    {
+        directions[0].SetActive(true);
+        directions[1].SetActive(false);
+        directions[2].SetActive(false);
+    }
+    public void EnableTop()
+    {
+        directions[0].SetActive(false);
+        directions[1].SetActive(true);
+        directions[2].SetActive(false);
+    }
+    public void EnableBottom()
+    {
+        directions[0].SetActive(false);
+        directions[1].SetActive(false);
+        directions[2].SetActive(true);
     }
 }
